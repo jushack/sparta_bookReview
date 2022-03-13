6412bd38ee5e989349242164cd3a77d8b45867d5
jushack <3.jushack.1@gmail.com>
2022-03-13 11:52:02 +0900

    bookreview

--- app.py
+++ app.py
@@ -33,6 +33,5 @@ def read_reviews():
     reviews = list(db.bookreview.find({}, {'_id': False}))
     return jsonify({'all_reviews': reviews})
 
-
 if __name__ == '__main__':
     app.run('0.0.0.0', port=5000, debug=True)
